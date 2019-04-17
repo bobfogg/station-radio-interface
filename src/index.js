dd3c7b23cdfc684e98376ad9fcbafb9008cd3fb8
bobfogg <bob.fogg@celltracktech.com>
2019-04-17 04:04:15 -0400

    current logfile /data/ctt.log

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { BaseStation } from './server/base_station';
 
-const DataFile = '/data/ctt/ctt.log';
+const DataFile = '/data/ctt.log';
 const LogFile = '/data/ctt/sensor-station.log';
 const station = new BaseStation({
   data_filename: DataFile
