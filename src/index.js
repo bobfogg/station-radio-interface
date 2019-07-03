d1148f43a8a28479721ba58352dd11feee3a9cec
bobfogg <bob.fogg@celltracktech.com>
2019-07-03 14:16:43 -0400

    default 90 sec display update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const station = new BaseStation({
   write_errors: false,
   flush_data_secs: 30,
   server_checkin_freq: 61 * 60,
-  update_screen_freq: 60,
+  update_screen_freq: 90,
   rotation_freq: 60 * 30, // 30 minute rotation
   upload_freq: 60 * 60, // hourly
 });
