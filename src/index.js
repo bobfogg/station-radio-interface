52d2dbc918c80b58c8c7dca6282c475e84e47a53
bobfogg <bob.fogg@celltracktech.com>
2019-07-09 17:12:40 -0400

    hourly server checkin, rotate, 123 min upload

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const station = new BaseStation({
   record_data: true,
   write_errors: false,
   flush_data_secs: 30,
-  server_checkin_freq: 61 * 60,
+  server_checkin_freq: 60 * 60,
   update_screen_freq: 90,
-  rotation_freq: 60 * 30, // 30 minute rotation
-  upload_freq: 60 * 60, // hourly
+  rotation_freq: 60 * 60,  // 60 minute rotation
+  upload_freq: 61.3 * 60 * 2, // hourly
 });
 station.start({});
